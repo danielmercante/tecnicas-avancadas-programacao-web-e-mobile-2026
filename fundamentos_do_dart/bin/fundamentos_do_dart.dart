482bdbf4e931487d1ca4c5154aa9c5714c02d7ae
Daniel <danielmercante7@gmail.com>
2026-03-23 19:27:48 +0000

    introdução ao Dart

---
+++ fundamentos_do_dart/bin/fundamentos_do_dart.dart
@@ -0,0 +1,5 @@
+import 'package:fundamentos_do_dart/fundamentos_do_dart.dart' as fundamentos_do_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fundamentos_do_dart.calculate()}!');
+}
---
+++ fundamentos_do_dart/test/fundamentos_do_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:fundamentos_do_dart/fundamentos_do_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
